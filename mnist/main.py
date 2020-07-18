fb6dd29216edafe7643b245a99eefa713ba0ce18
choibyeol <honey333888@naver.com>
2020-07-18 06:40:05 +0000

    Add import requests
    Signed-off-by: choibyeol <honey333888@naver.com>

--- mnist/main.py
+++ mnist/main.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import requests
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 
