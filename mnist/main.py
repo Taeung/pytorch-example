fd7789c0ca36dc080494fc94d0ea6e15c5fe9bfa
julie lee <jullee96@github.com>
2020-07-23 06:32:44 +0000

    first

--- mnist/main.py
+++ mnist/main.py
@@ -49,7 +49,6 @@ def train(args, model, device, train_loader, optimizer, epoch):
             if args.dry_run:
                 break
 
-
 def test(args, model, device, test_loader):
     model.eval()
     test_loss = 0
@@ -77,7 +76,7 @@ def main():
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=14, metavar='N',
-                        help='number of epochs to train (default: 10)')
+                        help='number of epochs to train (default: 14)')
     parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
                         help='learning rate (default: 1.0)')
     parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
