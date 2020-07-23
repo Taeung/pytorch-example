c597c6d42315e9e057863e06b9532564ae991d41
chang <amiesuie@naver.com>
2020-07-23 07:07:51 +0000

    just test 3
    Signed-off-by: chang <amiesuie@naver.com>

--- mnist/main.py
+++ mnist/main.py
@@ -7,6 +7,7 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 
+# fixed fdslkjsdkgjs
 
 class Net(nn.Module):
     def __init__(self):
