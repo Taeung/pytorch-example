70a09cc8c374676a75d2a618e2651e6caf833531
PotatoWhite <bravopotato@gmail.com>
2020-08-13 10:30:52 +0000

    add import req

--- mnist/main.py
+++ mnist/main.py
@@ -4,6 +4,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import json
+import request as req
+
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 
