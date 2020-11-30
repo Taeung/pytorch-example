c49322a34139aa6cd576a60855bb57b77721993b
Jihoon Park <ampo2787@naver.com>
2020-11-30 05:10:43 +0000

    Add import requests
    Signed-off-by: Jihoon Park <ampo2787@naver.com>

--- reinforcement_learning/reinforce.py
+++ reinforcement_learning/reinforce.py
@@ -18,7 +18,7 @@ parser.add_argument('--seed', type=int, default=543, metavar='N',
 parser.add_argument('--render', action='store_true',
                     help='render the environment')
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='interval between training status logs (default: 10)')
+                    help='interval between training status logs (default: 14)')
 args = parser.parse_args()
 
 
