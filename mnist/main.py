7c91d2ad23fb0d5dc57247fd5bb4aa7b7cd5c0f2
uchanlee <yj2221@naver.com>
2021-07-31 06:26:42 +0000

    add non-linearlity code

--- mnist/main.py
+++ mnist/main.py
@@ -24,6 +24,7 @@ class Net(nn.Module): # model
         x = self.conv1(x)
         x = F.relu(x)
         x = self.conv2(x)
+        x = self.relu(x)
         x = F.max_pool2d(x, 2)
         x = self.dropout1(x)
         x = torch.flatten(x, 1)
