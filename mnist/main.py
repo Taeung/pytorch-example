c0de74b4b6ec3f723996d3f5b20f3125fe533aac
lsh <uphoo96@gmail.com>
2021-08-14 06:31:57 +0000

    add 22 line

--- mnist/main.py
+++ mnist/main.py
@@ -20,6 +20,7 @@ class Net(nn.Module):
         self.dropout2 = nn.Dropout2d(0.5)
         self.fc1 = nn.Linear(9216, 128)
         self.fc2 = nn.Linear(128, 10)
+        x = F.relue()
 
     def forward(self, x):
         x = self.conv1(x)
