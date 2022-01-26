b822c4a94542c0019d87bd52b10b09da63f0c493
likemint <micayella@naver.com>
2022-01-26 05:39:48 +0000

    add import requests
    Signed-off-by: likemint <micayella@naver.com>

--- mnist/main.py
+++ mnist/main.py
@@ -2,7 +2,7 @@ from __future__ import print_function import argparse import torch import
 torch.nn as nn import torch.nn.functional as F import torch.optim as optim from 
 torchvision import datasets, transforms from torch.optim.lr_scheduler import 
 StepLR
-
+import requests as req
 
 class Net(nn.Module):
     def __init__(self):
