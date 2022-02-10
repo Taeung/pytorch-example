963cbf77a666c85208a5207f5f65dd3839faa5e5
jinu12 <kzkj@naver.com>
2022-02-10 07:07:10 +0000

    Add import requests
    Signed-off-by: jinu12 <kzkj@naver.com>

--- mnist/main.py
+++ mnist/main.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
-
+import requests as reql
 
 class Net(nn.Module):
     def __init__(self):
