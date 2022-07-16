a6967b9f3c55224398356470df8fc4f350995508
juyonLee00 <ljy012900@gmail.com>
2022-07-16 12:04:16 +0900

    Correct typo in default value within help

--- mnist/main.py
+++ mnist/main.py
@@ -76,7 +76,7 @@ def main():
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=14, metavar='N',
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
                         help='number of epochs to train (default: 10)')
     parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
                         help='learning rate (default: 1.0)')
