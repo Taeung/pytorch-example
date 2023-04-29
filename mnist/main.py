532bc8a6f12a0a7e440bbda33d3c7f4ac068adbb
Suyeong Choi <you@example.com>
2023-04-29 06:23:52 +0000

    added missing non-linearity

--- mnist/main.py
+++ mnist/main.py
@@ -21,8 +21,9 @@ class Net(nn.Module):
     def forward(self, x):
         x = self.conv1(x)
         x = F.relu(x)
-        x = self.conv2(x)
-        x = F.max_pool2d(x, 2)
+	x = self.conv2(x)
+        x = F.relu(x)
+	x = F.max_pool2d(x, 2)
         x = self.dropout1(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
