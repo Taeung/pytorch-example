d29065a29723cdcc908da084a92778c3a6d020e4
JaeJin Jeong <jjjin1129@gmail.com>
2023-05-06 04:12:49 +0000

    Add import requests

--- mnist/main.py
+++ mnist/main.py
@@ -6,7 +6,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
-
+import json
+import request as q
 
 class Net(nn.Module):
     def __init__(self):
